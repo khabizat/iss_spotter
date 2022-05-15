4a12b94658bac838439dc7ad75f5814bcf91e4b0
Khabizat Sharapudinova <habizat22@gmail.com>
2022-05-15 23:47:44 +0000

    File for runnung and testing functions

---
+++ index.js
@@ -0,0 +1,44 @@
+// index.js
+const { fetchMyIP, fetchCoordsByIP, fetchISSFlyOverTimes, nextISSTimesForMyLocation } = require('./iss');
+
+
+// fetchMyIP((error, ip) => {
+//   if (error) {
+//     console.log("It didn't work!" , error);
+//     return;
+//   }
+//   console.log(ip);
+// });
+
+// fetchCoordsByIP('24.203.176.64', (error, data) => {
+//   if (error) {
+//     console.log('It didn\'t work', error);
+//     return;
+//   }
+//   console.log(data);
+// });
+
+// fetchISSFlyOverTimes({ latitude: 45.54475021362305, longitude: -75.74067687988281 }, (error, data) => {
+//   if (error) {
+//     console.log('It didn\'t work', error);
+//     return;
+//   }
+//   console.log(data);
+// });
+
+const printPassTimes = function(passTimes) {
+  for (const pass of passTimes) {
+    const datetime = new Date(0);
+    datetime.setUTCSeconds(pass.risetime);
+    const duration = pass.duration;
+    console.log(`Next pass at ${datetime} for ${duration} seconds!`);
+  }
+};
+
+nextISSTimesForMyLocation((error, passTimes) => {
+  if (error) {
+    return console.log("It didn't work!", error);
+  }
+  // success, print out the deets!
+  printPassTimes(passTimes);
+});
